8c3c7c5f1b5dfec05a50d31d20a82dccffb27a0f
gardeljunior <saldanha351@gmail.com>
2021-04-07 18:58:24 -0300

    chore: add constants file with snacks data

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const SNACKS = [
+  { id: 1, name: "Coxinha" },
+  { id: 2, name: "Pastel de carne" },
+  { id: 3, name: "Pastel de queijo" },
+  { id: 4, name: "Esfirra" },
+  { id: 5, name: "Canudinho" },
+  { id: 6, name: "Empada" },
+  { id: 7, name: "Torta de Frango" }
+];
+
+export const SNACKS_BOX_PRICE = 49.9;
+export const SNACKS_BOX_MAX_QTY = 100;
